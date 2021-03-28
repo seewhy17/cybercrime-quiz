35b46c3c7e728f14eece0200071aac90521383ea
Cyril Fehintoluwa <cyrilcril@gmail.com>
2021-03-28 21:41:10 +0100

    added important messages on the homepage

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="bg-hero-image bg-cover min-h-screen w-full flex flex-col justify-center items-center">
   <h1 class="text-gray-50 text-4xl lg:text-6xl">Cybercrime Assessment</h1>
+  <p class="text-gray-50 text-lg font-thin mt-3">This assessments contains 20 multiple choice questions and should take about 10 minutes to complete</p>
+  <p class="text-gray-50 text-lg font-bold tracking-wider mt-3 underline">Please note that your answers are confidential and will not be shared with a third party</p>
   <router-link to="/quiz" class="py-2 px-4 bg-purple-500 text-gray-50 mt-6 text-xl font-medium">Start Quiz &gt;&gt;</router-link>
 
   </div>
